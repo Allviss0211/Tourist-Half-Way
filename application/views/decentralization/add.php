96d9d073dd8699b98f760b81376a95f5575d5dc6
nhulm1501 <nhuviolet2000@gmail.com>
2020-07-15 22:49:46 +0700

    View: User, Detralization

--- application/views/decentralization/add.php
+++ application/views/decentralization/add.php
@@ -1,22 +1,32 @@
-<?php echo form_open('decentralization/add',array("class"=>"form-horizontal")); ?>
-
-	<div class="form-group">
-		<label for="Name" class="col-md-4 control-label">Name</label>
-		<div class="col-md-8">
-			<input type="text" name="Name" value="<?php echo $this->input->post('Name'); ?>" class="form-control" id="Name" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Note" class="col-md-4 control-label">Note</label>
-		<div class="col-md-8">
-			<input type="text" name="Note" value="<?php echo $this->input->post('Note'); ?>" class="form-control" id="Note" />
-		</div>
-	</div>
-	
-	<div class="form-group">
-		<div class="col-sm-offset-4 col-sm-8">
-			<button type="submit" class="btn btn-success">Save</button>
-        </div>
-	</div>
-
-<?php echo form_close(); ?>
+<div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Thêm phân quyền</h3>
+            </div>
+            <?php echo form_open('decentralization/add'); ?>
+          	<div class="box-body">
+          		<div class="row clearfix">
+					<div class="col-md-6">
+						<label for="Name" class="control-label">Tên</label>
+						<div class="form-group">
+							<input type="text" name="Name" value="<?php echo $this->input->post('Name'); ?>" class="form-control" id="Name" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Note" class="control-label">Ghi chú</label>
+						<div class="form-group">
+							<input type="text" name="Note" value="<?php echo $this->input->post('Note'); ?>" class="form-control" id="Note" />
+						</div>
+					</div>
+				</div>
+			</div>
+          	<div class="box-footer">
+            	<button type="submit" class="btn btn-success">
+            		<i class="fa fa-check"></i> Lưu
+            	</button>
+          	</div>
+            <?php echo form_close(); ?>
+      	</div>
+    </div>
+</div>
--- application/views/user/add.php
+++ application/views/user/add.php
@@ -1,70 +1,84 @@
-<?php echo form_open('user/add',array("class"=>"form-horizontal")); ?>
-
-	<div class="form-group">
-		<label for="Username" class="col-md-4 control-label">Username</label>
-		<div class="col-md-8">
-			<input type="text" name="Username" value="<?php echo $this->input->post('Username'); ?>" class="form-control" id="Username" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Password" class="col-md-4 control-label">Password</label>
-		<div class="col-md-8">
-			<input type="text" name="Password" value="<?php echo $this->input->post('Password'); ?>" class="form-control" id="Password" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="IdDecentralization" class="col-md-4 control-label">IdDecentralization</label>
-		<div class="col-md-8">
-			<input type="text" name="IdDecentralization" value="<?php echo $this->input->post('IdDecentralization'); ?>" class="form-control" id="IdDecentralization" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Fullname" class="col-md-4 control-label">Fullname</label>
-		<div class="col-md-8">
-			<input type="text" name="Fullname" value="<?php echo $this->input->post('Fullname'); ?>" class="form-control" id="Fullname" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="IdentityCard" class="col-md-4 control-label">IdentityCard</label>
-		<div class="col-md-8">
-			<input type="text" name="IdentityCard" value="<?php echo $this->input->post('IdentityCard'); ?>" class="form-control" id="IdentityCard" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Birthday" class="col-md-4 control-label">Birthday</label>
-		<div class="col-md-8">
-			<input type="text" name="Birthday" value="<?php echo $this->input->post('Birthday'); ?>" class="form-control" id="Birthday" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Phone" class="col-md-4 control-label">Phone</label>
-		<div class="col-md-8">
-			<input type="text" name="Phone" value="<?php echo $this->input->post('Phone'); ?>" class="form-control" id="Phone" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Email" class="col-md-4 control-label">Email</label>
-		<div class="col-md-8">
-			<input type="text" name="Email" value="<?php echo $this->input->post('Email'); ?>" class="form-control" id="Email" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Address" class="col-md-4 control-label">Address</label>
-		<div class="col-md-8">
-			<input type="text" name="Address" value="<?php echo $this->input->post('Address'); ?>" class="form-control" id="Address" />
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="Note" class="col-md-4 control-label">Note</label>
-		<div class="col-md-8">
-			<input type="text" name="Note" value="<?php echo $this->input->post('Note'); ?>" class="form-control" id="Note" />
-		</div>
-	</div>
-	
-	<div class="form-group">
-		<div class="col-sm-offset-4 col-sm-8">
-			<button type="submit" class="btn btn-success">Save</button>
-        </div>
-	</div>
-
-<?php echo form_close(); ?>
+<div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Thêm người dùng</h3>
+            </div>
+            <?php echo form_open('user/add'); ?>
+          	<div class="box-body">
+          		<div class="row clearfix">
+					<div class="col-md-6">
+						<label for="Username" class="control-label">Tài khoản</label>
+						<div class="form-group">
+							<input type="text" name="Username" value="<?php echo $this->input->post('Username'); ?>" class="form-control" id="Username" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Password" class="control-label">Mật khẩu</label>
+						<div class="form-group">
+							<input type="text" name="Password" value="<?php echo $this->input->post('Password'); ?>" class="form-control" id="Password" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="IdDecentralization" class="control-label">Vai trò</label>
+						<div class="form-group">
+							<select name="IdDecentralization" class="form-control" id="IdDecentralization">
+								<?php foreach($decentralization as $role) { ?>
+									<option value="<?php echo $role['IdDecentralization'] ?>"><?php echo $role['Name'] ?></option>
+								<?php } ?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Fullname" class="control-label">Họ tên</label>
+						<div class="form-group">
+							<input type="text" name="Fullname" value="<?php echo $this->input->post('Fullname'); ?>" class="form-control" id="Fullname" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="IdentityCard" class="control-label">Chứng minh thư</label>
+						<div class="form-group">
+							<input type="text" name="IdentityCard" value="<?php echo $this->input->post('IdentityCard'); ?>" class="form-control" id="IdentityCard" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Birthday" class="control-label">Ngày sinh</label>
+						<div class="form-group">
+							<input type="date" name="Birthday" value="<?php echo $this->input->post('Birthday'); ?>" class="form-control" id="Birthday" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Phone" class="control-label">Số điện thoại</label>
+						<div class="form-group">
+							<input type="text" name="Phone" value="<?php echo $this->input->post('Phone'); ?>" class="form-control" id="Phone" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Email" class="control-label">Email</label>
+						<div class="form-group">
+							<input type="text" name="Email" value="<?php echo $this->input->post('Email'); ?>" class="form-control" id="Email" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Address" class="control-label">Địa chỉ</label>
+						<div class="form-group">
+							<input type="text" name="Address" value="<?php echo $this->input->post('Address'); ?>" class="form-control" id="Address" />
+						</div>
+					</div>
+					<div class="col-md-6">
+						<label for="Note" class="control-label">Ghi chú</label>
+						<div class="form-group">
+							<input type="text" name="Note" value="<?php echo $this->input->post('Note'); ?>" class="form-control" id="Note" />
+						</div>
+					</div>
+				</div>
+			</div>
+          	<div class="box-footer">
+            	<button type="submit" class="btn btn-success">
+            		<i class="fa fa-check"></i> Lưu
+            	</button>
+          	</div>
+            <?php echo form_close(); ?>
+      	</div>
+    </div>
+</div>
